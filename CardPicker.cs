595f2f4fb4918551f6881b8574e485094cf6ffec
Diego Mendonça Silva <diegomsbr@gmail.com>
2025-06-23 21:25:31 -0300

    Update card picker UI and implement card selection logic
    Modified MainWindow.xaml to change the title and layout, replacing existing controls with a new StackPanel for card selection. Added a Button to trigger card picking and a ListBox to display results.
    
    Implemented Button_Click event handler in MainWindow.xaml.cs to handle card selection and display.
    
    Introduced CardPicker.cs with methods to randomly generate card values and suits, allowing for dynamic card selection based on user input.

---
+++ CardPicker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickACardUI
+{
+    public class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards) {
+            string[] cards = new string[numberOfCards];
+
+            for (int i=0; i < numberOfCards; i++) {
+                string cardValue = RandomValue();
+                string cardSuit = RandomSuit();
+                cards[i] = $"{cardValue} of {cardSuit}";
+            }
+            return cards;
+        }
+        private static string RandomValue() {
+            int value = random.Next(1, 14); 
+            string cardValue = value switch
+            {
+                1 => "Ace",
+                11 => "Jack",
+                12 => "Queen",
+                13 => "King",
+                _ => value.ToString()
+            };
+            return cardValue;
+        }
+        private static string RandomSuit() {
+            string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+            int suitIndex = random.Next(0, suits.Length);
+            string cardSuit = suits[suitIndex];
+            return cardSuit;
+        }
+    }
+
+}
